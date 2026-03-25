44cdbb25be015014eead3941f3a0aadd262bd054
printify-backend <kendieasykenya@gmail.com>
2026-03-25 22:57:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://kendieasykenyafashionglobe.blogspot.com'
+  origin: 'https://kendieasykenyaglobal.blogspot.com'
 }));
 
 app.get('/', (req, res) => {
